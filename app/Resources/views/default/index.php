f09d41271b15c303e4e83cb7e8e898461abdcfeb
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-10-23 22:18:57 -0700

    delete authentication section due to errors

--- app/Resources/views/default/index.php
+++ app/Resources/views/default/index.php
@@ -2,16 +2,7 @@
   include("Config.php");
   include("Auth.php");
 
-  $dbh = new PDO("mysql:host=localhost;dbname=phpauth", "username", "password");
-
-  $config = new PHPAuth\Config($dbh);
-  $auth   = new PHPAuth\Auth($dbh, $config);
-  if(!$auth->isLogged()) {
-    header('HTTP/1.0 403 Forbidden');
-    echo "Forbidden";
-
-    exit();
-  }
+  
 ?>
 <!DOCTYPE html>
 <html>
