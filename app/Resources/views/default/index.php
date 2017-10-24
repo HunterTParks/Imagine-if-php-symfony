5048d5b355ef05bce6f8ed7c7debd3abf4fef441
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-10-23 18:55:21 -0700

    add template php page for testing Authenication plugin

---
+++ app/Resources/views/default/index.php
@@ -0,0 +1,14 @@
+<?php
+  include("Config.php");
+  include("Auth.php");
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Testing Title</title>
+  </head>
+  <body>
+    HEY THERE YOU GOT THERE
+  </body>
+</html>
