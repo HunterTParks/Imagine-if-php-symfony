96414467def37f9a951b2ceab6adf116f8a430be
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-10-25 15:52:57 -0700

    add a function to controller class to render the main login screen

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -6,6 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use PDO;
 
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginFunction(Request $request)
+    {
+      return $this->render('login/login.html.twig', array(
+          'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+      ));
+    }
+}
+
 
 
 ?>
