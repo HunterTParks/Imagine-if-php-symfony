42d1162966d66e3d6e9f9e1de119efba2f83fc4a
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-10-25 17:53:36 -0700

    add a line to load a namespace for using authentication

--- app/autoload.php
+++ app/autoload.php
@@ -5,6 +5,7 @@ use Composer\Autoload\ClassLoader;
 
 /** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
+$loader->add('AppBundle\Controller', __DIR__.'/../vendor/phpauth');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
