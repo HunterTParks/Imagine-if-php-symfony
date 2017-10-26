057d520c19c922c3cca7943662eb05153a8fc984
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-10-25 19:30:03 -0700

    add link to custom Entity User

--- app/autoload.php
+++ app/autoload.php
@@ -6,6 +6,7 @@ use Composer\Autoload\ClassLoader;
 /** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('AppBundle\Controller', __DIR__.'/../vendor/phpauth');
+$loader->add('AppBundle\Controller', __DIR__.'/../src/AppBundle/Entity/User');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
