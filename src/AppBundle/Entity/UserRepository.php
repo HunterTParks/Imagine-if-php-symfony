8fb190539231d45afc536d9cc180c6f2a7eaf366
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-10-28 01:39:19 -0700

    add symfony classes and the namespace to file

--- src/AppBundle/Entity/UserRepository.php
+++ src/AppBundle/Entity/UserRepository.php
@@ -1,3 +1,12 @@
 <?php
+    namespace AppBundle\Entity;
+
+    use Symfony\Component\Security\Core\User\UserInterface;
+    use Symfony\Component\Security\Core\User\UserProviderInterface;
+    use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+    use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+    use Doctrine\ORM\EntityRepository;
+    use Doctrine\ORM\NoResultException;
+
     
 ?>
