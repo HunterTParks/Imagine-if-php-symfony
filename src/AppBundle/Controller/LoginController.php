93fb61f2f1978bd6a0df80d881c3f75299556af6
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-10-28 00:03:01 -0700

    add authentication error message and last username entered variables into the login template

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use PDO;
 
 class LoginController extends Controller
@@ -11,10 +12,18 @@ class LoginController extends Controller
     /**
      * @Route("/login", name="login")
      */
-    public function loginFunction(Request $request)
+    public function loginFunction(Request $request, AuthenticationUtils $authUtils)
     {
+      //Get error | IF THERE IS ONE
+      $errorMessage = $authUtils->getLastAuthenticationError();
+
+      //Get Last username entered
+      $lastUsername = $authUtils->getLastUsername();
+
       return $this->render('login/login.html.twig', array(
           'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+          'last_username' => $lastUsername,
+          'error_message' => $errorMessage,
       ));
     }
 }
