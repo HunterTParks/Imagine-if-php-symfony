0538f69348e406a63ac862e1585e651ec89b0d60
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-10-30 15:25:42 -0700

    add the User Entity and the UserType form namespace into the Login Controller

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -5,6 +5,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use AppBundle\Form\UserType;
+use AppBundle\Entity\User;
 use PDO;
 
 class LoginController extends Controller
