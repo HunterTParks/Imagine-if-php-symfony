ceda96f0de31d95ce50753074fdb8f4451ab2087
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-10-30 17:17:59 -0700

    add UserRepository in the correct folder

---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,24 @@
+<?php
+    namespace AppBundle\Entity;
+
+    use Symfony\Component\Security\Core\User\UserInterface;
+    use Symfony\Component\Security\Core\User\UserProviderInterface;
+    use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+    use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+    use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+    use Doctrine\ORM\EntityRepository;
+    use Doctrine\ORM\NoResultException;
+
+    class UserRepository extends EntityRepository implements UserLoaderInterface
+    {
+        public function loadUserByUserName($username)
+        {
+          return $this->createQueryBuilder('u')
+                ->where('u.username = :username OR u.email = :email')
+                ->setParameter('username', $username)
+                ->setParameter('email', $usename)
+                ->getQuery()
+                ->getOneOrNullResult();
+        }
+    }
+?>
