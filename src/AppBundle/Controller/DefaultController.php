6e2fc05241173fa9b777141c9de7ec69d400daaa
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-11-01 01:33:43 -0700

    add a call to get all users from database into the guildlist function and push it into guildies template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -80,9 +80,11 @@ class DefaultController extends Controller
      */
     public function guildListAction(Request $request)
     {
-
+        $userRepo = new UserRepository();
+        $array = $userRepo->loadAllUsers();
         return $this->render('default/guildies.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'users' => $array,
         ));
     }
 
