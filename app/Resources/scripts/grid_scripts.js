627cd249d50a1637bb9abd019d3cee8018e5d667
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-11-01 17:47:37 -0700

    add jsgrid scripts

---
+++ app/Resources/scripts/grid_scripts.js
@@ -0,0 +1,26 @@
+$(function(){
+    var db = {
+        loadData: function(filter) {
+            return $.grep(this.users, function(user) {
+                return (!filter.username || user.username.indexOf(filter.username) > -1);
+            });
+        }
+    }
+
+    db.users = "<?php echo $users ?>";
+
+    $("#grid").jsGrid({
+      filtering: true,
+      editing: false,
+      sorting: true,
+      autoload: true,
+
+      controller: "<?php echo $users ?>",
+
+      fields: [
+        { name: "username", type: "text", textField: "Username" },
+        { name: "email", type: "text", textField: "Email" },
+        { name: "isActive", type: "number", textField: "is Active" }
+      ]
+    });
+});
