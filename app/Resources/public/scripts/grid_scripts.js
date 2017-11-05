235d9d9a6be46cf36851a7b678b8efc83b569d64
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-11-05 09:39:25 -0800

    add jsgrid complete files to project and edit grid_scripts to take new project files

--- app/Resources/public/scripts/grid_scripts.js
+++ app/Resources/public/scripts/grid_scripts.js
@@ -1,26 +1,27 @@
-$(function(){
-    var db = {
-        loadData: function(filter) {
-            return $.grep(this.users, function(user) {
-                return (!filter.username || user.username.indexOf(filter.username) > -1);
-            });
-        }
+$(document).ready(function(){
+  aler("testing");
+  var db = {
+    loadData: function(filter) {
+      return $.grep(this.users, function(user) {
+        return (!filter.username || user.username.indexOf(filter.username) > -1);
+      });
     }
+  }
 
-    db.users = "<?php echo $users ?>";
+  db.users = "<?php echo $users ?>";
 
-    $("#grid").jsGrid({
-      filtering: true,
-      editing: false,
-      sorting: true,
-      autoload: true,
+  $("#grid").jsGrid({
+    filtering: true,
+    editing: false,
+    sorting: true,
+    autoload: true,
 
-      controller: "<?php echo $users ?>",
+    controller: "<?php echo $users ?>",
 
-      fields: [
-        { name: "username", type: "text", textField: "Username" },
-        { name: "email", type: "text", textField: "Email" },
-        { name: "isActive", type: "number", textField: "is Active" }
-      ]
-    });
+    fields: [
+      { name: "username", type: "text", textField: "Username" },
+      { name: "email", type: "text", textField: "Email" },
+      { name: "isActive", type: "number", textField: "is Active" }
+    ]
+  });
 });
